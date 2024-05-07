244c7ac049e78d9742ba4ee45a5fd22cde81fc9e
Abdallah Alnajar <140744532+abdallahalnagar188@users.noreply.github.com>
2024-05-08 01:43:41 +0200

    fix some error v3

--- app/src/main/java/com/example/islamiproject/ui/home/sebha/ConstantsSebha.kt
+++ app/src/main/java/com/example/islamiproject/ui/home/sebha/ConstantsSebha.kt
@@ -2,9 +2,9 @@ package com.example.islamiproject.ui.home.sebha
 
 class ConstantsSebha {
     companion object{
-        val SOBHAN_ALLAH : String = "سبحان الله"
-        val ALHAMDLLAH : String = "الحمد لله"
-        val ALLAH_AKBER : String = "الله اكبر"
-        val LAELAHELA_ALLAH:String = "لا اله الا الله وحده لا شريك له له الملك وله الحمد وهو علي كل شيء قدير"
+        const val SOBHAN_ALLAH : String = "سبحان الله"
+        const val ALHAMDLLAH : String = "الحمد لله"
+        const val ALLAH_AKBER : String = "الله اكبر"
+        const val LAELAHELA_ALLAH:String = "لا اله الا الله وحده لا شريك له له الملك وله الحمد وهو علي كل شيء قدير"
     }
 }
--- app/src/main/java/com/example/islamiproject/ui/home/sebha/SebhaFragment.kt
+++ app/src/main/java/com/example/islamiproject/ui/home/sebha/SebhaFragment.kt
@@ -1,15 +1,12 @@
 package com.example.islamiproject.ui.home.sebha
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import com.example.islamiproject.R
-import com.example.islamiproject.databinding.ActivityHomeScreenBinding
+import androidx.fragment.app.Fragment
 import com.example.islamiproject.databinding.FragmentSebhaBinding
 
 class SebhaFragment : Fragment() {
@@ -43,28 +40,32 @@ class SebhaFragment : Fragment() {
         }
 
     }
-    fun onClickImageSeb7a(){
-        imageSeb7a.rotation = imageSeb7a.rotation+5
+    private fun onClickImageSeb7a(){
+        imageSeb7a.rotation += 5
         counter++
         tvCounter.text="$counter"
         if (tvTasbeh.text == ConstantsSebha.LAELAHELA_ALLAH){
             counter = 0
             tvCounter.text = "$counter"
         }
-        if(counter == 33) {
-            if (tvTasbeh.text == ConstantsSebha.SOBHAN_ALLAH)
-                tvTasbeh.text = ConstantsSebha.ALHAMDLLAH
-            tvCounter.text = "$counter"
+        when (counter) {
+            33 -> {
+                if (tvTasbeh.text == ConstantsSebha.SOBHAN_ALLAH)
+                    tvTasbeh.text = ConstantsSebha.ALHAMDLLAH
+                tvCounter.text = "$counter"
 
-        }else if(counter == 66){
-            if(tvTasbeh.text == ConstantsSebha.ALHAMDLLAH)
-            tvTasbeh.text = ConstantsSebha.ALLAH_AKBER
-            tvCounter.text = "$counter"
-        }else if(counter==99){
-           if(tvTasbeh.text == ConstantsSebha.ALLAH_AKBER)
-            tvTasbeh.text = ConstantsSebha.LAELAHELA_ALLAH
-            counter =0
-            tvCounter.text = "$counter"
+            }
+            66 -> {
+                if(tvTasbeh.text == ConstantsSebha.ALHAMDLLAH)
+                    tvTasbeh.text = ConstantsSebha.ALLAH_AKBER
+                tvCounter.text = "$counter"
+            }
+            99 -> {
+                if(tvTasbeh.text == ConstantsSebha.ALLAH_AKBER)
+                    tvTasbeh.text = ConstantsSebha.LAELAHELA_ALLAH
+                counter =0
+                tvCounter.text = "$counter"
+            }
         }
 
     }
